34303e50ef7669cd68cd7dd794acdaf9477fdfe4
giblets2570 <tomkeohanemurray@gmail.com>
2015-08-23 21:39:08 +0100

    added maps

--- script.js
+++ script.js
@@ -3,42 +3,72 @@
 // int X, Y;
 
 int speed = 8;
-int dirX, dirY;
-Wall[] walls;
+ArrayList walls;
 Hero hero;
+Food food;
 
 // int[] numbers
 // boolean hit;
 
 
+//defining the shape of the level
+
+level = [ "wwwwwwwwwwwwwwwwwwww",
+          "w    w             w",
+          "w    w             w",
+          "w    w             w",
+          "w    wwwwww        w",
+          "w                  w",
+          "w                  w",
+          "w                  w",
+          "w                  w",
+          "w           w      w",
+          "w           w      w",
+          "wwwwwwwwwwwww      w",
+          "w                  w",
+          "w                  w",
+          "wwwwwwwwwwwwwwwwwwww"];
+
+
 // Setup the Processing Canvas
 void setup(){
-  size( 500, 500 );
-  strokeWeight( 1 );
+  size( 800, 600 );
+  strokeWeight( 0 );
   frameRate( 60 );
-  walls = {new Wall(0,0,width,16),new Wall(0,0,16,height),new Wall(0,height-16,width,16),new Wall(width-16,0,16,height)};
-  hero = new Hero(200, 200, 8, 1, 0, 20.0);
+  walls = new ArrayList();
+  hero = new Hero(200, 400, 8, 1, 0, 30.0);
+  food = new Food();
 }
 
 // Main draw loop
 void draw(){
 
 	// Fill canvas grey
-	background( 100 );
+	background(183,191,200);
 
 	// Set fill-color to blue
-	fill( 0, 121, 184 );
+	fill( 37,39,164 );
 
 	// Set stroke-color white
 	stroke(255);
 
 	// Draw circle
 	// ellipse( X, Y, diameter, diameter );
-	for(int i = 0; i < walls.length; i++){
-		walls[i].draw();
-	}
+  for(int i = 0; i < level.length; i++){
+    // walls[i].draw();
+    for(int j = 0; j < level[i].length; j++){
+      // println(level[i][j]=='w');
+      // rect(j*40,i*40,40,40);
+      if(level[i][j] == "w"){
+        rect(j*40,i*40,40,40);
+      }
+    }
+  }
+
 	hero.update();
 	hero.draw();
+
+  food.draw();
 
 }
 
@@ -62,22 +92,25 @@ void keyPressed(){
     }
 }
 
-class Wall {
+class Food {
+  int x, y;
 
-  float x, y, w, h;
+  Food(){
+    this.x = int(random(20, width - 20));
+    this.y = int(random(20, height - 20));
+  }
 
-  Wall (float x, float y, float w, float h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
+  void changePos(){
+    this.x = int(random(20, width - 20));
+    this.y = int(random(20, height - 20));
   }
 
   void draw() {
-    rect(x,y,w,h);
+    fill(37,39,51);
+    ellipse(this.x,this.y,10,10);
   }
 
-};
+}
 
 class Hero {
 
@@ -97,28 +130,30 @@ class Hero {
   void update(){
   	this.x += this.dirX*this.speed;
   	this.y += this.dirY*this.speed;
-  	boolean hit = false;
 
-  	if(this.x - this.diameter < this.diameter / 2){
-  		hit = true;
-  	}
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 0;
+    int y2 = 0;
 
-  	if(this.x + this.diameter > width - this.diameter / 2){
-  		hit = true;
-  	}
+    if(dirY == 0){
+      x1 = int((this.x + dirX*diameter / 2) / 40);
+      x2 = int((this.x + dirX*diameter / 2) / 40);
+      y1 = int((this.y + diameter / 2) / 40);
+      y2 = int((this.y - diameter / 2) / 40);
 
-  	if(this.y - this.diameter < this.diameter / 2){
-  		hit = true;
-  	}
 
-  	if(this.y + this.diameter > height - this.diameter / 2){
-  		hit = true;
-  	}
+    }else if(dirX == 0){
+      y1 = int((this.y + dirY*diameter / 2) / 40);
+      y2 = int((this.y + dirY*diameter / 2) / 40);
+      x1 = int((this.x + diameter / 2) / 40);
+      x2 = int((this.x - diameter / 2) / 40);
+    }
 
-  	if(hit){
-  		this.x -= this.dirX*this.speed;
-  		this.y -= this.dirY*this.speed;
-  	}
+    if(level[y1][x1] == "w" || level[y2][x2] == "w"){
+      this.x -= this.dirX*this.speed;
+      this.y -= this.dirY*this.speed;
+    }
 
   }
   void draw() {
